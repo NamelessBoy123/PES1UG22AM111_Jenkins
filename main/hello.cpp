9a12d39d7f07cc53dc66b28945fd2d0f4c9d08bc
Pavankumar <148214543+NamelessBoy123@users.noreply.github.com>
2025-03-15 10:16:23 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "webhook connected successfully\n";
+ cout << "I succesfully build the 2 job \n";
+  // cout << "webhook connected successfully\n";
   return 0;
 }
